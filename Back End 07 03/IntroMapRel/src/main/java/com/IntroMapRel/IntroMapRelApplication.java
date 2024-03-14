b742eb683c09e04f45c4789969df0b22c84b4475
LuizGuilhermers <132360055+LuizGuilhermers@users.noreply.github.com>
2024-03-14 14:13:19 -0300

    Add files via upload

---
+++ Back End 07 03/IntroMapRel/src/main/java/com/IntroMapRel/IntroMapRelApplication.java
@@ -0,0 +1,13 @@
+package com.IntroMapRel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroMapRelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroMapRelApplication.class, args);
+	}
+
+}
---
+++ Back End 07 03/IntroMapRelATIV/src/main/java/IntroMapRelATIV/IntroMapRelAtivApplication.java
@@ -0,0 +1,13 @@
+package IntroMapRelATIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroMapRelAtivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroMapRelAtivApplication.class, args);
+	}
+
+}
